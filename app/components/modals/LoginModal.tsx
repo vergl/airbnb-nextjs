2ba7e3425306b4a4a8b90e201c5f23081120e0d7
Stanislav Giley <stanislavgiley@MacBook-Pro-Stanislav-2.local>
2023-04-11 16:38:49 +0300

    finish category selection

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -20,6 +20,7 @@ const LoginModal = () => {
     const registerModal = useRegisterModal();
     const loginModal = useLoginModal();
     const router = useRouter();
+
     const [isLoading, setIsLoading] = useState(false);
 
     const {
@@ -56,6 +57,11 @@ const LoginModal = () => {
             }
         })
     }
+
+    const toggle = useCallback(() => {
+        loginModal.onClose();
+        registerModal.onOpen();
+    },[loginModal, registerModal]);
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
@@ -101,13 +107,13 @@ const LoginModal = () => {
             <div className="text-neutral-500 text-center mt-4 font-light">
                 <div className="justify-center flex flex-row items-center gap-2">
                     <div>
-                        Already have an account?
+                        First time using Airbnb?
                     </div>
                     <div 
-                        onClick={registerModal.onClose}
+                        onClick={toggle}
                         className="text-neutral-800 cursor-pointer hover:underline"
                     >
-                        Login
+                        Create an account
                     </div>
                 </div>
             </div>
