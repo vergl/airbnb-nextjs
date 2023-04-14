45888847afb27a0dfd62c534bff18ac9a8a95f62
Stanislav Giley <stanislavgiley@MacBook-Pro-Stanislav-2.local>
2023-04-14 08:55:55 +0300

    finish listings component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,46 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Container from './components/Container'
+import EmptyState from './components/EmptyState';
+import getListings from './actions/getListings';
+import ListingCard from './components/listings/ListingCard';
+import getCurrentUser from './actions/getCurrentUser';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <EmptyState showReset />
+    )
+  }
+
   return (
-    <div className='text-rose-500 text-2xl'>Hello Airbnb</div>
+    <Container>
+      <div className="
+        pt-24 
+        grid 
+        grid-cols-1 
+        sm:grid-cols-2 
+        md:grid-cols-3 
+        lg:grid-cols-4 
+        xl:grid-cols-5 
+        2xl:grid-cols-6 
+        gap-8
+      "
+      >
+        {listings.map((listing: any) => {
+          return (
+            <ListingCard 
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          )
+        })}
+      </div>
+    </Container>
   )
 }
